587abe8b6858dd7d8b85dedb6eb97485c32a040d
Jose Altamira <jarmandoalt@gmail.com>
2022-12-04 04:01:07 -0600

    dotenv add

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//require("dotenv").config();
+require("dotenv").config();
 const app = require("./app");
 const connectdb = require("./db/mongodb");
 const { dbConfig, discord } = require("./config");
